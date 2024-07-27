9822844edb8c1a221a893da365e1a3b0e4cac77c
ardevi44 <arbey9621@gmail.com>
2024-07-27 13:57:58 -0600

    Simple for iterations in python

---
+++ playing-loops-iterations.py
@@ -0,0 +1,20 @@
+numbers = [1,2,3,4,5,6]
+# Where i is 0
+for i in numbers:
+  print(f"i is equal to {i}")
+  
+print("\nSome other example\n")
+
+# The last value is not included, is one before the value of the second parameter
+# Show the count 1 to 10 but if we find with 5 we break
+for i in range(1,11):
+  print(i)
+  if i == 5:
+    break
+
+# If we find 5 we continue to the next evaluation loop the code bellow will not executed
+print("\n\nAnother example.")
+for i in range(1,11):
+  if i == 5:
+    continue
+  print(i)
